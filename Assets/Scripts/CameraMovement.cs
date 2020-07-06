09cbf641509690e745ffd47053d3ccf4ac33bd74
Inshades <realityinshades@gmail.com>
2020-07-06 21:57:13 +0530

    Intialized stalls

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// /// This script for the camera movement
+/// </summary>
+public class CameraMovement : MonoBehaviour
+{
+    public float MoveSpeed;    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(MoveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, MoveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+    }
+}
