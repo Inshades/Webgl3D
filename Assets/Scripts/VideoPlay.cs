868648624d117bb85a4519a59ef01c8efe92c8ac
Inshades <realityinshades@gmail.com>
2020-07-15 21:54:48 +0530

    Demo Project Build

---
+++ Assets/Scripts/VideoPlay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+public class VideoPlay : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public string name;
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, name);
+        videoPlayer.Play();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
